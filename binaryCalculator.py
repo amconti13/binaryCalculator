f55bfe0150eea9fbd1bcfd29765c345b08cda58d
amconti13 <amconti13@aol.com>
2016-09-09 12:55:16 -0400

    Final update to binaryCalculator

--- binaryCalculator.py
+++ binaryCalculator.py
@@ -1,41 +1,52 @@
-print( "This program acts like a calculator for binary numbers.\n")
-again = None
-while again != "x":
-    print( "Please write numbers in two's complement 8-bit form and choose\n "
-          + "x, -, *, / as the operators. (Eg: 00000000 + 11111111)" )
-    str = raw_input( "Please input operation: ")
-            #just input() does that actual operation in full.
-    #str = "00000101 + 000100000"
+# Created by Arianna Conti 9/9/16
+import datetime
+date = datetime.date.today()
+print ("------------------------------------------------------------------\n"
+       + "Arianna Conti\t\t\t\t\t " + str(date)
+       + "\nCSCI 3351\t\t\t\t\t Binary Calculator\n"
+       + "------------------------------------------------------------------\n"
+       + "This program is a calculator for binary numbers.\n")
 
-    splitStr = str.split( ' ' ) #Split string by spaces
+again = "$n00py"
+while again != "x":
+    str = raw_input( "Please write numbers in two's complement 8-bit form and"
+                    + " choose\nx, -, *, / as the operators. (Eg: 00000000 +"
+                    + " 11111111)\nPlease input operation: ")
+    
+#Split string by spaces
+    splitStr = str.split( ' ' )
     num1 = splitStr[ 0 ]
     op = splitStr[ 1 ]
     num2 = splitStr[ 2 ]
 
-    num1 = int( num1, 2 )#string to int
+#string to int
+    num1 = int( num1, 2 )
     num2 = int( num2, 2 )
 
-    if op == "+":
-        x = num1 + num2 #add
-    elif op == "-":
-        x = num1 - num2 #subtract
-    elif op == "*":
-        x = num1 * num2 #mutliply
+#if negative, get value of number from two's compliment binary
+    if num1 > 127 : num1 = -(( 0b11111111 - num1 ) + 1 )
+    if num2 > 127 : num2 = -(( 0b11111111 - num2 ) + 1 )
+
+#calculate
+    if  op == "+": x = num1 + num2 #add
+    elif op == "-": x = num1 - num2 #subtract
+    elif op == "*": x = num1 * num2 #mutliply
     elif op == "/":
-        if num2 == 0:
-            x = "Can't divide by 0" #undefined
+        if num2 == 0: x = "Cannot divide by 0" #undefined
+        else: x = num1 / num2 #divide
+    else: x = "Invalid operator"
+
+#if answer is an int
+    if isinstance( x, int ):
+        if x < -128 or x > 127: #overflow
+            x = "Overflow has occured, cannot calculate."
+        elif x < 0 :
+            x = (( 0b11111111 + x ) + 1 )#change to two's complement
+            x = "{0:08b}".format( x )#int to binary with 0's instead of 0b
         else:
-            x = num1 / num2 #divide
-    else:
-        x = "Invalid operator"
+            x = "{0:08b}".format( x )
 
-    if x < -127 or x > 127: #check for overflow
-        x =  "Overflow has occured, cannot calculate."
-
-    if isinstance( x, int ):        #if x is of type integer
-        x = "{0:08b}".format( x )   #int to binary with 0's instead of 0b
-
-    print(x)
-
-    again = input("Press any key to do another calculation, or press x to "
-                  + "exit.: ")
+#output and prompt
+    print("Answer: " + x)
+    again = raw_input("Enter any key to do another calculation, "
+                      + "or enter x to exit.: ")
